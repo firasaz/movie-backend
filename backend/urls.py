e2b75e722eaa0c605899aab6b0374f6ee3fcc5be
firasaz <firas.abuzahra2002@gmail.com>
2024-11-26 20:27:12 -0800

    added handshake to home route

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from api.views import handshake
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', handshake)
 ]
